f55723064e0cc35e259c9a20a8aaf21994d0abaf
Eduardo Díaz <iamedu@gmail.com>
2014-09-10 17:22:21 -0500

    MArathon engine factory

---
+++ jsr223/src/main/groovy/gex/marathon/jsr223/MarathonEngineFactory.groovy
@@ -0,0 +1,45 @@
+package gex.marathon.jsr223
+
+import javax.script.ScriptEngineFactory
+import javax.script.ScriptEngine
+
+class MarathonEngineFactory implements ScriptEngineFactory {
+
+  String getEngineName() {
+  }
+
+  String getEngineVersion() {
+  }
+
+  List<String> getExtensions() {
+  }
+
+  String getLanguageName() {
+  }
+
+  String getLanguageVersion() {
+  }
+
+  String getMethodCallSyntax(String obj, String m, String... args) {
+  }
+
+  List<String> getMimeTypes() {
+  }
+
+  List<String> getNames() {
+  }
+
+  String getOutputStatement(String toDisplay) {
+  }
+
+  Object getParameter(String key) {
+  }
+
+  String getProgram(String... statements) {
+  }
+
+  ScriptEngine getScriptEngine() {
+  }
+
+}
+

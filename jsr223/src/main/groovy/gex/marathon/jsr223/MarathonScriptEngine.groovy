f5df8afe2a9921fdd9c1e272569fc77c1b874cd6
Eduardo Díaz <iamedu@gmail.com>
2014-09-11 03:48:06 -0500

    Implemented a very simple script engine

--- jsr223/src/main/groovy/gex/marathon/jsr223/MarathonScriptEngine.groovy
+++ jsr223/src/main/groovy/gex/marathon/jsr223/MarathonScriptEngine.groovy
@@ -1,5 +1,6 @@
 package gex.marathon.jsr223
 
+import groovy.transform.CompileStatic
 import gex.marathon.core.MarathonRunner
 
 import javax.script.AbstractScriptEngine
@@ -7,19 +8,25 @@ import javax.script.Bindings
 import javax.script.ScriptContext
 import javax.script.ScriptEngineFactory
 
+@CompileStatic
 class MarathonScriptEngine extends AbstractScriptEngine {
 
   private MarathonEngineFactory factory
+  private MarathonRunner runner
 
   MarathonScriptEngine(MarathonEngineFactory factory) {
     super()
     this.factory = factory
+    this.runner = new MarathonRunner()
   }
 
   Object eval(String code, ScriptContext context) {
+    println context
+    runner.eval(code)
   }
 
   Object eval(Reader reader, ScriptContext context) {
+    eval(reader.text, context)
   }
 
   ScriptEngineFactory getFactory() {
